5ba99da3d09d2cf9a1785cdea3d5924c3026ea5d
Hiroyoshi Takahashi <t.hiroyoshi209@gmail.com>
2015-11-18 14:57:35 +0900

    update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     library: "ReactDnDItem",
     libraryTarget: "umd"
   },
+  resolve: { "root": "./src" },
   externals: { "react": "React" },
   module: { loaders: [{ test: /\.jsx?$/, loader: "babel", exclude: /node_modules/ }] }
 };
